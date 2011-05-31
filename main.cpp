e1f63da4bb942acaefc837e3630e2e747b4033e8
Ives van der Flaas <ives.vdf@gmail.com>
2011-05-31 18:22:48 +0200

    Some more info

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include "woodcutter.h"
 
 void doStuff();
+void doMoreStuff();
 
 int main()
 {
 	Woodcutter::minloglevel(INFO);
 	Woodcutter::setColors(true);
 
+	// Comment this out to test redirecting to file. 
 	doStuff();
 
 	// Or you can redirect to file:
 	std::ofstream f("output.txt");
 	Woodcutter::output(&f);
-	LOGI("From now on this will be in a file");
-	LOGF("Fatal!");
+
+	doMoreStuff();
 }
 
 void doStuff()
@@ -26,4 +28,12 @@ void doStuff()
 	LOG(WARNING, "The flux capacitor is in the red!");
 	LOG(FATAL, "The flux capacitor just blew!");
 	LOGE("Shorthand for Error logging");
+	// Everything afther last statement will *not* be executed, LOGF stands
+	// for log FATAL which exits the program. Use with care. 
+}
+
+void doMoreStuff()
+{
+	LOGI("From now on this will be in a file");
+	LOGF("Fatal!");
 }
